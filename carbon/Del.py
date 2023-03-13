6a10e831ba790b1cbee0bce39ed42f493ff256f5
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-03-13 11:41:41 +0530

    V-5

---
+++ carbon/Del.py
@@ -0,0 +1,24 @@
+import inspect
+
+import logging
+
+import sys
+
+import re
+
+from pathlib import Path
+
+from telethon import events
+
+def register(**args):
+
+    """ Registers a new message. """
+
+    pattern = args.get("pattern", None)
+
+    r_pattern = r"^[/!.]"
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+
+
+
